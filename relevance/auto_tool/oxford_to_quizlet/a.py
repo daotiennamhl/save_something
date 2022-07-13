ca62dddfa5df13c42f864303dd49d22af912723c
Nam <daotiennamhl120699@gmail.com>
2022-07-13 15:31:28 +0700

    update

--- relevance/auto_tool/oxford_to_quizlet/a.py
+++ relevance/auto_tool/oxford_to_quizlet/a.py
@@ -26,19 +26,23 @@ browser.maximize_window()
 # const
 text_file = 'text.txt'
 result_file = 'result.md'
+log_file = 'log.txt'
 words = []
 
 # clear file content
 writeToFile(result_file, '', '', 'w')
+writeToFile(log_file, '', '', 'w')
 
 with open(text_file, 'r', encoding="utf-8") as f:
     rows = f.read().split('\n')
-    words = map(lambda r: r.split(' ')[0], rows)
 
-for i, word in enumerate(words):
+for i, row in enumerate(rows):
+    if (len(row.split(' ', 1)) >= 2):
+        word, de = row.split(' ', 1)
+    else: word = row
     browser.get(getLink(word))
     try: 
-        waitElement('//h1[@class="headword"]', browser, 8)
+        waitElement('//h1[@class="headword"]', browser, 5)
         
         # word, prn, mean, *ex
         prnAm = browser.find_element_by_class_name('phons_n_am')
@@ -47,15 +51,16 @@ for i, word in enumerate(words):
         listEx = browser.find_element_by_class_name('examples')
         ex = listEx.find_elements_by_class_name('x')[:]
         ex = list(map(lambda e: e.text, ex))
-        if (len(ex) < 2): ex.append('')
 
-        model = Model(i, word, prn, mean, *ex)
+        if (len(ex) < 2): ex.append('')
+        if (prn in de): prn = ''
+
+        model = Model(i, word, de, prn, mean, *ex)
         writeToFile(result_file, model.toString(), '', 'a')
         print(i)
         sleep(0.1)
     except Exception as e:
         print('errr = ')
-        writeToFile('log.txt', f'{i} {traceback.format_exc()}' )
-        break
+        writeToFile(log_file, f'{i}. {word}\n{traceback.format_exc()}' )
 
 browser.close()
--- relevance/auto_tool/oxford_to_quizlet/model.py
+++ relevance/auto_tool/oxford_to_quizlet/model.py
@@ -1,14 +1,15 @@
 class Model:
-  def __init__(self, i, word, prn, mean, *ex):
+  def __init__(self, i, word, de, prn, mean, *ex):
     self.i = i
     self.word = word
+    self.de = de
     self.prn = prn
     self.mean = mean
     self.ex = ex
   def toString(self):
     return f'''
 {self.i}. {self.word}:
-{self.prn}
+{self.prn} - {self.de}
 {self.mean}
 - {self.ex[0]}
 - {self.ex[1]}
