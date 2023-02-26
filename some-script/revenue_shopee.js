583efa6e7cfee58a79bbf572cd47415320cdde4c
daotiennamhl <43399562+daotiennamhl@users.noreply.github.com>
2023-02-26 09:43:39 +0700

    Update revenue_shopee.js

--- some-script/revenue_shopee.js
+++ some-script/revenue_shopee.js
@@ -1,8 +1,9 @@
 let margin = 0.3;
 let sum = 0;
+let totalQty = 0;
 const panel = document.getElementsByClassName("shop-search-result-view")[0];
-let priceElement = panel.getElementsByClassName("_3TJGx5");
-let quantityElement = panel.getElementsByClassName("_2Tc7Qg");
+let priceElement = panel.getElementsByClassName("_2Ul3BO");
+let quantityElement = panel.getElementsByClassName("_36z98S");
 for (let i = 0; i < priceElement.length; i++) {
   let price = priceElement[i]?.innerText;
   let quantity = quantityElement[i]?.innerText;
@@ -15,6 +16,7 @@ for (let i = 0; i < priceElement.length; i++) {
     ? parseInt(quantity)
     : quantity.split("k")[0].replace(",", ".") * 1000;
   sum += price * quantity;
-  console.log(price, quantity);
+  totalQty += quantity;
+  console.log(price, totalQty);
 }
 console.log(sum, sum * margin);
